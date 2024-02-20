d09748950eedc9f2486db3d8e553ef8529fbca35
Vitaly <vitaly.neumoin@gmail.com>
2024-02-20 23:31:19 +0300

    Create basic number guessing game in Rust
    Created a new Rust project with a basic number guessing game. The user is prompted to guess a random number between 1 and 100. Dependencies for random number generation are declared in Cargo.toml and utilized in the main.rs file. Also, .gitignore file is added to ignore the /target directory.

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+mod test_async;
+
+use std::cmp::Ordering;
+use std::io;
+use rand::{Rng};
+
+fn main() {
+    println!("Загадайте число!");
+    let mut local_rng = rand::thread_rng();
+
+    let secret_number = local_rng.gen_range(1..=100);
+
+    loop {
+        println!("Пожалуйста введите число от 1 до 100.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Ошибка чтения строки");
+
+        println!("Вы загадали: {guess}");
+
+        let guess: u32 = guess.trim().parse().expect("Вы вписали не число!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Слишком низкое число"),
+            Ordering::Greater => println!("Слишком высокое число"),
+            Ordering::Equal => {
+                println!("Вы отгадали!");
+                return;
+            }
+        }
+    }
+}
