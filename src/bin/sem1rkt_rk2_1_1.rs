860173d798ba07ac473133ab590570898cc23f44
Vitaly <vitaly.neumoin@gmail.com>
2024-02-25 14:35:22 +0300

    Update 'sem1rkt_rk2_1_1.rs' for better input handling
    Refactored and divided the input function into 'scan_data' and 'scan_array' for better processing in 'sem1rkt_rk2_1_1.rs'. This change enables more straightforward array input, eliminating the need to specify the array size separately.

--- src/bin/sem1rkt_rk2_1_1.rs
+++ src/bin/sem1rkt_rk2_1_1.rs
@@ -2,27 +2,25 @@ use std::io;
 use std::str::FromStr;
 use num_traits::Num;
 
-fn scan_number<T: Num + FromStr<Err=std::num::ParseIntError>>() -> T {
+fn scan_data() -> String {
     let mut n = String::new();
     io::stdin().read_line(&mut n).expect("Не удалось распознать ввод");
-    let n: T = n.trim().parse().expect("Вы вписали не число!");
     return n;
 }
 
-fn input() -> Vec<i64> {
-    println!("Введите размер массива:");
-    let n: usize = scan_number();
-    let mut vec = Vec::new();
-    vec.resize(n, 0);
-    println!("Введите массив через разрыв строки:");
-    for i in 0..n {
-        vec[i] = scan_number();
+fn scan_array<T: Num + FromStr<Err=std::num::ParseIntError>>() -> Vec<T> {
+    let data = scan_data();
+    let arr = data.trim().split(" ");
+    let mut vec: Vec<T> = Vec::new();
+    for v in arr {
+        vec.push(v.parse().expect("Вы вписали не числа!"));
     }
     return vec;
 }
 
 fn main() {
-    let mut vec = input();
+    println!("Введите массив через пробел:");
+    let mut vec: Vec<i64> = scan_array();
     println!("Введённый массив: {:?}", vec);
     vec.retain(|&x| x % 2 != 0);
     println!("Обработанный массив: {:?}", vec);
